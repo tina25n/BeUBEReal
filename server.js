7f5664907ab40f79f19d5fdf700e4de563e9e3df
mvelve7 <michaelvelve@Michaels-MacBook-Air.local>
2024-05-19 04:49:33 -0700

    set up server to retrieve images and dummy html file to test

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-'use strict'
+"use strict";
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+const htttpServer = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+htttpServer.listen(PORT, () => {
+  console.log(`Server is listenning on Port ${3000}`);
+});
+app.get("/", express.static(path.join(__dirname, "./public")));
--- src/models/model.mjs
+++ src/models/model.mjs
@@ -1,53 +1,60 @@
-"use strict"; 
-import https from 'https'; 
-import geoip from 'geoip-lite';
-export default class Person 
-{
-  constructor(name, age, orientation, personality,
-    desiredAge, desiredPersonality, location, pets, desiredPets) {
+"use strict";
+import https from "https";
+import geoip from "geoip-lite";
+export default class Person {
+  constructor(
+    name,
+    age,
+    orientation,
+    personality,
+    desiredAge,
+    desiredPersonality,
+    location,
+    pets,
+    desiredPets
+  ) {
     this.name = name;
     this.age = age;
-    this.locationPromise = location; 
+    this.locationPromise = location;
     this.orientation = orientation; // sexual orientation
     this.personality = personality; // introvert, extrovert, both
     this.pets = pets;
     this.desiredAge = desiredAge;
     this.desiredPersonality = desiredPersonality;
     this.desiredPets = desiredPets;
+  }
 
-  }  
-
-  async getPersonLocation(){
-    const location = await this.fetchPersonLocation(); 
-    return location; 
+  async getPersonLocation() {
+    const location = await this.fetchPersonLocation();
+    return location;
   }
 
   fetchPersonLocation() {
     return new Promise((resolve, reject) => {
-      https.get(`https://api.ipify.org?format=json`, (res) => {
-        let data = '';
-        console.log("chunk has been loaded"); 
-        res.on('data', (chunk) => {
-          data += chunk; 
-        }); 
+      https
+        .get(`https://api.ipify.org?format=json`, (res) => {
+          let data = "";
+          console.log("chunk has been loaded");
+          res.on("data", (chunk) => {
+            data += chunk;
+          });
 
-        res.on("end", () => {
-          const parsedData = JSON.parse(data); 
-          const geo = geoip.lookup(parsedData.ip); 
-          console.log(geo.city); 
-          resolve(geo.city); 
-        }); 
-      })
-      .on("error", (err) => {
-        console.log(`Error is ${err.message}`)
-        reject(); 
-      }); 
-  })
+          res.on("end", () => {
+            const parsedData = JSON.parse(data);
+            const geo = geoip.lookup(parsedData.ip);
+            console.log(geo.city);
+            resolve(geo.city);
+          });
+        })
+        .on("error", (err) => {
+          console.log(`Error is ${err.message}`);
+          reject();
+        });
+    });
   }
 }
 
-class Matcher 
-{
+class Matcher {
   constructor() {
     // Properties
     this.locationPeopleDict = {}; // key, value = location, list of people
@@ -61,19 +68,19 @@ class Matcher
 
   // issues solving:
   // are they catfishing?
-  
+
   // Get similarities:
-    // Check if person's location is already a key 
-    //      - if it is, then add a person to the list
-    //      - if not, create new key and add person there
-    // Then call updateSimilaritiesField()
+  // Check if person's location is already a key
+  //      - if it is, then add a person to the list
+  //      - if not, create new key and add person there
+  // Then call updateSimilaritiesField()
   updateSimilaritiesLocation(person) {
     const location = person.locationPromise;
     if (location in this.locationPeopleDict) {
-        this.locationPeopleDict[location].push(person);
+      this.locationPeopleDict[location].push(person);
     } else {
-        this.locationPeopleDict[location] = new Array();
-        this.locationPeopleDict[location].push(person); 
+      this.locationPeopleDict[location] = new Array();
+      this.locationPeopleDict[location].push(person);
     }
     this.updateSimilaritiesField(person);
   }
@@ -81,72 +88,121 @@ class Matcher
   updateSimilaritiesField(newPerson) {
     const people_list = this.locationPeopleDict[newPerson.locationPromise];
     for (const existingPerson of people_list) {
-        if (existingPerson !== newPerson) {
-          // Instantiate matches list for both people if DNE
-            if (!this.matches.has(newPerson)) {
-                this.matches.set(newPerson, []);
-            }
-            if (!this.matches.has(existingPerson)) {
-                this.matches.set(existingPerson, []);
-            }
-            // Add to each other's matching list
-            if (this.areMatched(newPerson, existingPerson)) {
-                this.matches.get(newPerson).push(existingPerson);
-                this.matches.get(existingPerson).push(newPerson);
-            }
+      if (existingPerson !== newPerson) {
+        // Instantiate matches list for both people if DNE
+        if (!this.matches.has(newPerson)) {
+          this.matches.set(newPerson, []);
         }
+        if (!this.matches.has(existingPerson)) {
+          this.matches.set(existingPerson, []);
+        }
+        // Add to each other's matching list
+        if (this.areMatched(newPerson, existingPerson)) {
+          this.matches.get(newPerson).push(existingPerson);
+          this.matches.get(existingPerson).push(newPerson);
+        }
+      }
     }
+  }
+
+  areMatched(person1, person2) {
+    const lowerBoundOne = person1.desiredAge - 3;
+    const upperBoundOne = person1.desiredAge + 3;
+    const lowerBoundTwo = person2.desiredAge - 3;
+    const upperBoundTwo = person2.desiredAge + 3;
+
+    let score = 0;
+    if (person2.age >= lowerBoundOne && person2.age <= upperBoundOne) {
+      score += 2;
+    }
+    if (person1.age >= lowerBoundTwo && person1.age <= upperBoundTwo) {
+      score += 2;
+    }
+    if (person1.desiredPersonality == person2.personality) {
+      score += 2;
+    }
+    if (person2.desiredPersonality == person1.personality) {
+      score += 2;
+    }
+    if (person2.pets == person1.desiredPets) {
+      score += 1;
+    }
+    if (person1.pets == person2.desiredPets) {
+      score += 1;
+    }
+
+    return score >= 6;
+  }
 }
 
-
-    areMatched(person1, person2) {
-      const lowerBoundOne = person1.desiredAge - 3;
-      const upperBoundOne = person1.desiredAge + 3;
-      const lowerBoundTwo = person2.desiredAge - 3;
-      const upperBoundTwo = person2.desiredAge + 3;
-
-      let score = 0
-      if (person2.age >= lowerBoundOne && person2.age <= upperBoundOne) {
-        score += 2;
-      }
-      if (person1.age >= lowerBoundTwo && person1.age <= upperBoundTwo) {
-        score += 2;
-      }
-      if (person1.desiredPersonality == person2.personality) {
-        score += 2;
-      }
-      if (person2.desiredPersonality == person1.personality) {
-        score += 2;
-      }
-      if (person2.pets == person1.desiredPets) {
-        score += 1;
-      }
-      if (person1.pets == person2.desiredPets) {
-        score += 1
-      }
-
-      return score >= 6;
-      
-    }
-
-}
+class photoHandle {}
 
 const matcher = new Matcher();
 
 // TESTS:
 // constructor for reference:
-//name, age, orientation, personality, desiredAge, desiredPersonality, 
+//name, age, orientation, personality, desiredAge, desiredPersonality,
 // location, pets, desiredPets
 
 // Vancouver
-const person1 = new Person('Jake', 15, "straight", "introvert", 15, "outrovert", "Vancouver", "yes", "yes")
-const person2 = new Person('Bob', 15, "bisexual", "introvert", 15, "introvert", "Vancouver", "no", "yes")
+const person1 = new Person(
+  "Jake",
+  15,
+  "straight",
+  "introvert",
+  15,
+  "outrovert",
+  "Vancouver",
+  "yes",
+  "yes"
+);
+const person2 = new Person(
+  "Bob",
+  15,
+  "bisexual",
+  "introvert",
+  15,
+  "introvert",
+  "Vancouver",
+  "no",
+  "yes"
+);
 // Anne's in the right location but not much else is in the desired range:
-const person3 = new Person('Anne', 100, "straight", "introvert", 100, "introvert", "Vancouver", "no", "no" )
+const person3 = new Person(
+  "Anne",
+  100,
+  "straight",
+  "introvert",
+  100,
+  "introvert",
+  "Vancouver",
+  "no",
+  "no"
+);
 
 // Edmonton
-const person4 = new Person('Allie', 20, "straight", "outrovert", 23, "outrovert", "Edmonton", "yes", "yes")
-const person5 = new Person('Cam', 22, "straight", "outrovert", 28, "introvert", "Edmonton", "no", "yes")
+const person4 = new Person(
+  "Allie",
+  20,
+  "straight",
+  "outrovert",
+  23,
+  "outrovert",
+  "Edmonton",
+  "yes",
+  "yes"
+);
+const person5 = new Person(
+  "Cam",
+  22,
+  "straight",
+  "outrovert",
+  28,
+  "introvert",
+  "Edmonton",
+  "no",
+  "yes"
+);
 
 matcher.addPerson(person1);
 matcher.addPerson(person2);
@@ -154,12 +210,7 @@ matcher.addPerson(person3);
 matcher.addPerson(person4);
 matcher.addPerson(person5);
 
-
 // console.log(matcher.locationPeopleDict);
 console.log("Here are the matches: ", matcher.matches);
 // console.log("This is for person1: ", matcher.matches[person1]);
 // console.log("This is for person2: ",  matcher.matches[person2]);
-
-  
-
-  
