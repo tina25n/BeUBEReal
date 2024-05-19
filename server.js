829a3cc097942df5cea68581653df5ff85e78213
mvelve7 <michaelvelve@Michaels-MacBook-Air.local>
2024-05-19 05:11:03 -0700

    [not working yet] set up post request on app

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 "use strict";
 const http = require("http");
 const path = require("path");
+const os = require("os");
 const fs = require("fs");
+const multer = require("multer");
 const express = require("express");
+const updated_dir = path.join(__dirname, `images`, `testImage`);
 
 const app = express();
 const htttpServer = http.createServer(app);
@@ -10,4 +13,40 @@ const PORT = process.env.PORT || 3000;
 htttpServer.listen(PORT, () => {
   console.log(`Server is listenning on Port ${3000}`);
 });
-app.get("/", express.static(path.join(__dirname, "./public")));
+
+app.get("/", express.static(path.join(updated_dir, "./public")));
+
+const handleError = (err, res) => {
+  res.status(500).contentType("text/plain").end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+  dest: `${updated_dir}`,
+  // you might also want to set some limits: https://github.com/expressjs/multer#limits
+});
+
+app.post(
+  "/upload",
+  upload.single("file" /* name attribute of <file> element in your form */),
+  (req, res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "./uploads/image.png");
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+      fs.rename(tempPath, targetPath, (err) => {
+        if (err) return handleError(err, res);
+
+        res.status(200).contentType("text/plain").end("File uploaded!");
+      });
+    } else {
+      fs.unlink(tempPath, (err) => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png files are allowed!");
+      });
+    }
+  }
+);
