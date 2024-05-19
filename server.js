87cc7069a354542819df041d4048e5ec083c1b21
mvelve7 <michaelvelve@Michaels-MacBook-Air.local>
2024-05-19 05:25:04 -0700

    reconfigured directory structure

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ const os = require("os");
 const fs = require("fs");
 const multer = require("multer");
 const express = require("express");
-const updated_dir = path.join(__dirname, `images`, `testImage`);
+const updated_dir = path.join(__dirname, `uploads`, `testImage`);
 
+// check that the actual directory exists or make one if it does not
+if (!fs.existsSync(updated_dir)) {
+  fs.mkdirSync(updated_dir, { recursive: true });
+}
+
+//setup the application logic in express
 const app = express();
-const htttpServer = http.createServer(app);
+const httpServer = http.createServer(app);
 const PORT = process.env.PORT || 3000;
-htttpServer.listen(PORT, () => {
-  console.log(`Server is listenning on Port ${3000}`);
+httpServer.listen(PORT, () => {
+  console.log(`Server is listening on Port ${PORT}`);
 });
 
-app.get("/", express.static(path.join(updated_dir, "./public")));
+app.use("/", express.static(path.join(__dirname, "public")));
+console.log("made it by the express_static call");
 
 const handleError = (err, res) => {
   res.status(500).contentType("text/plain").end("Oops! Something went wrong!");
@@ -30,7 +37,7 @@ app.post(
   upload.single("file" /* name attribute of <file> element in your form */),
   (req, res) => {
     const tempPath = req.file.path;
-    const targetPath = path.join(__dirname, "./uploads/image.png");
+    const targetPath = path.join(__dirname, "uploads", "image.png");
 
     if (path.extname(req.file.originalname).toLowerCase() === ".png") {
       fs.rename(tempPath, targetPath, (err) => {
